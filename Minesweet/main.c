f3f4ffcae08f8584ea449353fca02f685c8907b4
Andrew Bae <admin@pwner.kr>
2019-04-11 04:13:01 +0900

    MineLocation: add radar function

--- Minesweet/main.c
+++ Minesweet/main.c
@@ -1,11 +1,7 @@
 #pragma warning(disable: 4996)
 #include <windows.h>
 #include <stdio.h>
-
-#define MINE_TIMER_ADDRESS (0x0100579C)
-
-int MineReadTimer(HANDLE hProcess);
-int MineWriteTimer(HANDLE hProcess, DWORD time);
+#include "main.h"
 
 int main()
 {
@@ -28,6 +24,12 @@ int main()
 	scanf("%d", &time);
 	MineWriteTimer(hProcess, time);
 
+	while (1) {
+		Sleep(1000);
+		system("cls");
+		MineLocation(hProcess);
+	}
+
 	CloseHandle(hProcess);
 }
 
@@ -36,7 +38,7 @@ int MineReadTimer(HANDLE hProcess)
 	DWORD time;
 	ReadProcessMemory(
 		hProcess,
-		(LPCVOID)MINE_TIMER_ADDRESS,
+		(LPCVOID)TIMER_ADDRESS,
 		&time,
 		sizeof(DWORD),
 		NULL);
@@ -47,8 +49,83 @@ int MineWriteTimer(HANDLE hProcess, DWORD time)
 {
 	WriteProcessMemory(
 		hProcess,
-		(LPCVOID)MINE_TIMER_ADDRESS,
+		(LPCVOID)TIMER_ADDRESS,
 		&time,
 		sizeof(DWORD),
 		NULL);
+}
+
+int MineLocation(HANDLE hProcess)
+{
+	unsigned char mapTemp[286] = { NULL };
+	unsigned char map[MAP_HEIGHT][MAP_WIDTH] = { NULL };
+
+	ReadProcessMemory(
+		hProcess,
+		(LPCVOID)MAP_START_ADDRESS,
+		&mapTemp,
+		286,
+		NULL);
+
+	UINT z = 0;
+	for (UINT y = 0; y < MAP_HEIGHT; y++) {
+		for (UINT x = 0; x < MAP_WIDTH; x++) {
+			if (*(mapTemp + z) == ID_NEWLINE) {
+				z += 1;
+				while ((*(mapTemp + z) != ID_NEWLINE) && (z < MAP)) {
+					z += 1;
+				}
+				z += 1;
+				break;
+			}
+			*(*(map + y) + x) = *(mapTemp + z);
+			z += 1;
+		}
+	}
+	for (UINT y = 0; y < MAP_HEIGHT; y++) {
+		for (UINT x = 0; *(*(map + y) + x) != NULL; x++) {
+			switch (*(*(map + y) + x)) {
+			case ID_MINE:
+				printf("* ");
+				break;
+			case ID_MINE_FLAG:
+				printf("v ");
+				break;
+			case ID_UNKNOWN:
+				printf("? ");
+				break;
+			case ID_UNKNOWN_FLAG:
+				printf("v ");
+				break;
+			case ID_BACKGROUND:
+				printf("0 ");
+				break;
+			case (ID_BACKGROUND + 1):
+				printf("1 ");
+				break;
+			case (ID_BACKGROUND + 2):
+				printf("2 ");
+				break;
+			case (ID_BACKGROUND + 3):
+				printf("3 ");
+				break;
+			case (ID_BACKGROUND + 4):
+				printf("4 ");
+				break;
+			case (ID_BACKGROUND + 5):
+				printf("5 ");
+				break;
+			case (ID_BACKGROUND + 6):
+				printf("6 ");
+				break;
+			case (ID_BACKGROUND + 7):
+				printf("7 ");
+				break;
+			case (ID_BACKGROUND + 8):
+				printf("8 ");
+				break;
+			}
+		}
+		puts("");
+	}
 }
